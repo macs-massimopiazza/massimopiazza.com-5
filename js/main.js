24239d97f8a66b621082bee343f19c6167a9297f
macs-massimopiazza <me@massimopiazza.com>
2022-02-19 04:00:05 +0100

    footer done, main content done, dark mode switch done

--- js/main.js
+++ js/main.js
@@ -37,3 +37,53 @@ linkDone.addEventListener("click", function() {
     linkDone.classList.add("active");
     doneSection.classList.add("active");
 });
+
+//dark-theme switch
+const switchElement = document.getElementById("switch-container");
+const bodyElement = document.getElementsByTagName("body")[0];
+const stickmanImg = document.getElementsByClassName("stickman")[0];
+
+switchElement.addEventListener("click", function() { 
+    bodyElement.classList.toggle("dark");
+    if(bodyElement.classList[0] == "dark") {
+        stickmanImg.src="img/foto-sinistra-dark.png"
+    } else {
+        stickmanImg.src="img/foto-sinistra.png"
+    }
+    if(localStorage.getItem("dark-theme") == "false") {
+        localStorage.setItem("dark-theme", "true");
+    } else if (localStorage.getItem("dark-theme") == "true"){
+        localStorage.setItem("dark-theme", "false");
+    }
+});
+
+//save dark theme preference
+if (localStorage.getItem("dark-theme") == null) {
+    localStorage.setItem("dark-theme", "false")
+}
+if (localStorage.getItem("dark-theme") == "true") {
+    console.log("cambio a dark se variaile ok")
+    bodyElement.classList.add("dark");
+}
+
+//get time
+function getTheTime() {
+    let today = new Date();
+    let hour = today.getHours()
+    let minutes = today.getMinutes()
+    if(hour < 10) {
+        hour = "0" + hour;
+    }
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    hoursElement.innerHTML = hour;
+    minutesElement.innerHTML = minutes;
+}
+
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+
+getTheTime();
+setInterval(getTheTime, 20000);
+
